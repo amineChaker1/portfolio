2c51355e7685de7cec9100565d804789e608ef54
Chaker Amine <chkramine2003@gmail.com>
2025-02-27 19:47:48 +0100

    changed contact info

--- src/component/Contact/Contact.js
+++ src/component/Contact/Contact.js
@@ -109,11 +109,11 @@ function Contact() {
           Paris / Créteil <br />
           Phone: +33605920875
           <br />
-          Email: chkramine2003@gmail.com
+          Email: amchaker@outlook.com
         </div>
         <div className="map-wrap">
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2628.3296319640845!2d2.45540417686405!3d48.794687105186874!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e60cc9f2a6c8f1%3A0x2754888174437b63!2s11%20Rue%20Jean%20Jaur%C3%A8s%2C%2094000%20Cr%C3%A9teil!5e0!3m2!1sen!2sfr!4v1739812314832!5m2!1sen!2sfr"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d80717.75801964379!2d2.2646335809035154!3d48.85882554206043!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66e1f06e2b70f%3A0x40b82c3688c9460!2sParis!5e1!3m2!1sfr!2sfr!4v1740681923708!5m2!1sfr!2sfr"
             style={{ border: 0 }}
             className="myMap"
             allowfullscreen=""
